c92a2f8b7b90ecb57d575315de16b184802a9994
Sergei Antipov <mrantip90@gmail.com>
2025-12-10 22:45:22 +0300

    ДЗ Решение проблем

---
+++ src/main/java/org/example/DebugTask6.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class DebugTask6 {
+    public static void main(String[] args) {
+        countdown(5);
+    }
+
+    public static void countdown(int n) {
+        //метод вызывал сам себя бесконечно. Нет условия выхода из программы
+        //добавил while и умененьшение n
+        while (n >= 1) {
+            System.out.println(n);
+            n--;
+        }
+    }
+}
